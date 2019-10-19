4b292d1a3e93d3d8f5a7d6c8c2c4d82e5245243c
Ulysses <This2willpass@aol.com>
2019-10-19 02:18:45 -0700

    General HTML layout created

---
+++ assets/js/questions.js
@@ -0,0 +1,29 @@
+
+
+var questions = [
+    {
+      title: "Which actors did not star in the movie 'Avengers: Infinity War'?",
+      choices: ["Chris Evans", "Tom Holland", "Mel Gibson", "Zoe Saldana"],
+      answer: "Reese Witherspoon",
+    },
+    {
+      title: "The Greatest Showman is a biographical drama based on?", 
+      choices: ["Abraham Lincoln", "P.T. Barnum", "Shakespear", "Elvis"],
+      answer: "P.T. Barnum",
+    },
+    {
+      title: "IN the movie, Toy Story, what name is written on Woody's shoe?", 
+      choices: ["Peter", "Christopher", "Timmy", "Andy"],
+      answer: "Andy",
+    },
+    {
+      title: "Who directed the blockbuster movie Avatar?", 
+      choices: ["Steven Spielberg", "Michael Bay", "James Cameron", "Ron Howard"],
+      answer: "James Cameron",
+    },
+    {
+      title: "In the movie, The Dark Knight Rises, which actor starred in the role of Bane?", 
+      choices: ["Tom Hardy", "Christian Bale", "Gary Oldman", "Heath Ledger"],
+      answer: "Tom Hardy",
+    },
+  ];
---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+
+
+    var timeleft = 75;
+    var downloadTimer = setInterval(function(){
+    timeleft--;
+    document.getElementById("countdowntimer").textContent = timeleft;
+    if(timeleft <= 0)
+        clearInterval(downloadTimer);
+    },1000);
+
+
+    
